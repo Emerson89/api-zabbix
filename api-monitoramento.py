14ce64b42f6137db175a07947773cbde3cde52fa
Emerson <you@example.com>
2022-02-22 18:26:43 -0300

    add modulos

---
+++ api-monitoramento.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+from main import Monit
+import getpass
+import sys
+
+api = Monit("http://192.168.33.10","Admin","zabbix")
+
+def menu():
+    print("---- Menu opções Hosts ----")
+    print("[1] - Get Total Hosts")
+    print("[2] - Get Itens não suportados")
+    print("[3] - Create users")
+    geral()
+
+def geral():
+    opcao = input("Select an option: ")
+
+    if opcao == '1':
+     api.procura_hosts()
+   
+    elif opcao == '2':
+     api.procura_itens()
+
+    elif opcao == '3':
+       api.procurando_groupusers()
+       api.createUserfromCSV("users.csv")
+
+menu()
+
--- main.py
+++ main.py
@@ -1,17 +1,99 @@
-from zabbix_api import ZabbixAPI
+from zabbix_api import ZabbixAPI,Already_Exists
 import sys
-import getpass
+import csv
 
-def login():
- URL = 'http://192.168.33.10'
- USERNAME = 'Admin'
- PASSWORD = 'zabbix'
+class Monit(ZabbixAPI):
+  def __init__(self, URL, USERNAME, PASSWORD):
+   self.zapi = self.conect_api(URL, USERNAME, PASSWORD)
 
- try:
+  def conect_api(self, URL, USERNAME, PASSWORD): 
+   try:
     zapi = ZabbixAPI(URL, timeout=15)
     zapi.login(USERNAME, PASSWORD)
     print(f'Conectado na API do Zabbix, Versao Atual {zapi.api_version()}')
- except Exception as err:
+   except Exception as err:
     print(f'Falha ao conectar na API do zabbix, erro: {err}')
- 
- return zapi
+  
+   return zapi
+
+  def procura_hosts(self):
+     ids = self.zapi.host.get({
+        "output": ['hostid','name'],
+        "selectInterfaces": ["interfaceid", "ip"],
+        "selectGroups": "extend",
+     })
+     if ids:
+        for x in ids:
+         print (x)
+        print()
+        opcao = input("\nDeseja gerar relatorio em arquivo? [s/n]")
+        if opcao == 's' or opcao == 'S':
+            with open('hostsids.csv', 'w', newline='') as arquivo_csv:
+               fieldnames = ['Hostid', 'Name', 'Grupo', 'Interfaces']
+               escrever = csv.DictWriter(arquivo_csv, delimiter=';', fieldnames=fieldnames)
+               escrever.writeheader()
+            for x in ids:
+               with open('hosts.csv', 'a') as arquivo_csv:
+                escrever = csv.writer(arquivo_csv, delimiter=';')
+                escrever.writerow([x['hostid'],x['name'],x['groups'],x['interfaces']])
+     else:
+        print("***Hosts não encontrado***")
+     self.zapi.logout()
+  
+  def procura_itens(self):
+     itens = self.zapi.item.get({"output": "extend", "monitored": "true", "filter": {"state": 1}})
+     
+     for x in itens:
+            print("HOSTID: {},ITEMID: {},KEY: {},NOME: {},ERROR:{}".format(x["hostid"], x["itemid"], x["key_"], x["name"],x["error"]))
+     print()
+     print("Total de itens não suportados: ", len(itens))
+     opcao = input("\nDeseja gerar relatorio em arquivo? [s/n]")
+     if opcao == 's' or opcao == 'S':
+            with open('itens.csv', 'w', newline='') as arquivo_csv:
+               fieldnames = ['Hostid', 'ItemID', 'Key', 'Nome', 'Error']
+               escrever = csv.DictWriter(arquivo_csv, delimiter=';', fieldnames=fieldnames)
+               escrever.writeheader()
+            for x in itens:
+               with open('itens.csv', 'a') as arquivo_csv:
+                escrever = csv.writer(arquivo_csv, delimiter=';')
+                escrever.writerow([x['hostid'],x['itemid'],x['key_'],x['name'],x['error']])
+     self.zapi.logout()
+
+  def procurando_groupusers(self):
+    group_ids = input("Pesquise o nome do grupo de usuario: ")
+    ids = self.zapi.usergroup.get({
+        "output": ['name','usrgrpid'],
+        "sortfield": "name",
+        "search": {"name": '*' + group_ids + '*'},
+        "searchWildcardsEnabled": True
+    })
+    if ids:
+        print("***GroupsUsers encontrados***")
+        print()
+        for x in ids:
+            print (x['name'],"-", x['usrgrpid'])            
+    else:
+        print("***GroupsUsers não encontrados***")
+  print()
+
+  def create_user(self, user, password):
+    GROUPID = input("Insira o groupid...: ")
+    try:
+       create_user = self.zapi.user.create({
+           "alias": user,
+           "passwd": password,
+           "usrgrps": [{"usrgrpid":GROUPID}],
+       })
+       self.zapi.logout()
+
+       print(f'User cadastrado {user}')
+    except Already_Exists:
+       print(f'User(s) já cadastrado {user}')
+    except Exception as err:
+       print(f'Falha ao cadastrar user {err}')
+
+  def createUserfromCSV(self, fileName):
+      with open(fileName) as file:
+         file_csv = csv.reader(file, delimiter=';')
+         for [nome,senha] in file_csv:
+            self.create_user(user=nome,password=senha)
